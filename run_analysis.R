a7d7f007d1fcad30c108a480e9eb336b673b14cf
Sofiane <sofiane1ben@yahoo.fr>
2023-12-17 21:58:17 +0100

    Final solution

--- run_analysis.R
+++ run_analysis.R
@@ -1,107 +1,94 @@
+# The R script will perform the following tasks:
+# 1. Merge the training and test sets to create one data set
+# 2. Extracts measurements on the mean and standard deviation for each measurement
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names
+# 5. From the data set in Step 4, create a second, independent tidy data set with
+#.   the average of each variable for each activity and each subject
+
+
+
 rm(list=ls())
 library('lubridate')
 library('dplyr')
 library('stringr')
 library('tidyr')
+library('data.table')
 
 ##############################################################################
-####### 1 - Merges the training and the test sets to create one data set#####
+####### Merges the training and the test sets to create one data set#####
 ##############################################################################
 
 
 ### Import train data set
-trainx <- read.table("./Dataset/train/X_train.txt",header=FALSE)
-trainy <- read.table("./Dataset/train/y_train.txt",header=FALSE)
-subjectX<- read.table("./Dataset/train/subject_train.txt",header=FALSE)
-names(trainy)="y"
-names(subjectX)="subject"
+activity_labels <- fread("./Dataset/activity_labels.txt",header = FALSE, 
+                         col.names = c("activityID", "activityNames"))
+features <- fread("./Dataset/features.txt",header = FALSE,
+                  col.names = c("featureID", "featureNames"))
+
+
+trainx <- fread("./Dataset/train/X_train.txt",header=FALSE, 
+                col.names= features$featureNames)
+trainy <- fread("./Dataset/train/y_train.txt",header=FALSE, 
+                col.names = "Activity")
+subjectX<- fread("./Dataset/train/subject_train.txt",header=FALSE, 
+                 col.names = "SubjectID")
 
 ## Merge the X and Y columns for train data
-train <- cbind(subjectX,trainx,trainy)
-train$group <- "train"
+train <- cbind(subjectX,trainy,trainx)
+
 ## remove the intermediate data set
 rm(subjectX,trainx,trainy)
 
 ### import test data set
-testx <- read.table("./Dataset/test/X_test.txt",header=FALSE)
-testy <- read.table("./Dataset/test/y_test.txt",header=FALSE)
-subjecty <- read.table("./Dataset/test/subject_test.txt",header=FALSE)
+testx <- fread("./Dataset/test/X_test.txt",header=FALSE,
+                    col.names= features$featureNames)
 
-names(testy)="y"
-names(subjecty)="subject"
+testy <- fread("./Dataset/test/y_test.txt",header=FALSE,
+                    col.names = "Activity")
+subjecty <- fread("./Dataset/test/subject_test.txt",header=FALSE,
+                       col.names = "SubjectID")
+
 
 
 ## Merge the X and Y columns for train data
-test <- cbind(subjecty,testx,testy)
-test$group <- "test"
+test <- cbind(subjecty,testy,testx)
+
+data <- rbind(train,test)
+
 ## remove the intermediate data set
-rm(subjecty,testx,testy)
+rm(subjecty,test,train,testx,testy)
 
-data <- tibble::as_tibble(rbind(test,train))
-
-features <- read.table("./Dataset/features.txt",header = FALSE)
-names(data) <- c("subject",features$V2,"y","group")
-activity_label <- read.table("./Dataset/activity_labels.txt",header = FALSE)
 
 
 ##############################################################################
-####### 2 - Extracts the measurements on the mean and standard deviation #####
+#######  Extracts the measurements on the mean and standard deviation #####
 ##############################################################################
 
-## extract columns having mean and std in their names
-## + subject, y and group columns
-
-DataMeanStd <- data[c(1,grep("mean()|std()",names(data)))]
-
-## Remove MeanFreq out of scope
-DataMeanStd <- DataMeanStd[-grep("meanFreq()",names(DataMeanStd))]
-names(DataMeanStd)
+tidydata <- data %>% 
+  select(SubjectID, Activity, contains("mean()"), contains("std()"))
 
 ##############################################################################
-####### 3 -  descriptive activity names  in the data set ##### #############
+####### descriptive activity names in the data set and label ##### ############
 ##############################################################################
 
-names(DataMeanStd) <- str_replace(names(DataMeanStd),"^t","Time_")
-names(DataMeanStd) <- str_replace(names(DataMeanStd),"^f","Freq_")
-names(DataMeanStd) <- str_replace(names(DataMeanStd),"BodyAcc","Body_accelerometer")
-names(DataMeanStd) <- str_replace(names(DataMeanStd),"GravityAcc","Gravity_accelerometer")
-names(DataMeanStd) <- str_replace(names(DataMeanStd),"BodyGyro","Body_gyroscope")
-names(DataMeanStd) <- str_replace(names(DataMeanStd),"\\(\\)","")
-names(DataMeanStd) <- str_replace_all(names(DataMeanStd),"\\-","_")
+tidydata$Activity <- activity_labels[tidydata$Activity, 2]
 
-
-##############################################################################
-####### 4 - labels the data set with descriptive variable names #############
-##############################################################################
-
-data_final <- gather(DataMeanStd,"descriptive","Value",-subject)
-
-
-data_final$statistics <- str_extract(data_final$descriptive,"mean|std")
-table(data_final$statistics,useNA="ifany")
-
-data_final$domain_signals  <- str_extract(data_final$descriptive,"Time|Freq")
-table(data_final$domain_signals,useNA="ifany")
-
-data_final$capture  <- str_extract(data_final$descriptive,"Body|Gravity")
-table(data_final$capture,useNA="ifany")
-
-data_final$instruments  <- str_extract(data_final$descriptive,"accelerometer|gyroscope")
-table(data_final$instruments,useNA="ifany")
-
-data_final$axis  <- str_extract(data_final$descriptive,"X|Y|Z|JerkMag|Mag")
-table(data_final$axis,useNA="ifany")
+names(tidydata) <- gsub("^t", "Time", names(tidydata))
+names(tidydata) <- gsub("^f", "Frequency", names(tidydata))
+names(tidydata) <- gsub("Acc", "Accelerometer", names(tidydata))
+names(tidydata) <- gsub("Gyro", "Gyroscope", names(tidydata))
+names(tidydata) <- gsub("BodyBody", "Body", names(tidydata))
+names(tidydata) <- gsub("Mag", "Magnitude", names(tidydata))
+names(tidydata) <- gsub("()", "", names(tidydata))
 
 ###############################################################################
-####### 5 - average of each variable for each activity and each subject. ######
+####### average of each variable for each activity and each subject. ######
 ###############################################################################
 
 
-dataAvg <- data_final %>%
-  group_by(instruments,domain_signals,capture, axis, statistics,subject) %>%
-  summarize(count = n(),
-            avg_value = mean(Value)
-  )
+tidyMeans <- tidydata %>% group_by(SubjectID, Activity) %>% 
+  summarise_all(funs(mean))
 
-write.table(dataAvg,file="tidydata.txt",row.name=FALSE)
 
+fwrite(tidyMeans, file = "tidy_data.txt")
